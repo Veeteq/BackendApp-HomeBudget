b6afb72ae41c2e0b06a1250646e38990bc0c4ada
Veeteq <witold.wojnarowicz@gmail.com>
2023-07-13 17:46:00 +0200

    Enable Eureka Discovery client

--- src/main/java/com/veeteq/finance/budget/HomeBudgetApp.java
+++ src/main/java/com/veeteq/finance/budget/HomeBudgetApp.java
@@ -3,12 +3,14 @@ package com.veeteq.finance.budget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.veeteq.finance.budget.model")
 @EnableJpaRepositories(basePackages = "com.veeteq.finance.budget.repository")
+@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.veeteq.finance.budget.integration")
 public class HomeBudgetApp {
 
