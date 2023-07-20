b3f3421b836f5f4a62102e9305ebdd0c03b00398
Veeteq <witold.wojnarowicz@gmail.com>
2023-07-20 19:31:54 +0200

    CR-017: Provide list of unique Document Titles

--- src/main/java/com/veeteq/finance/budget/repository/BudgetDocumentRepository.java
+++ src/main/java/com/veeteq/finance/budget/repository/BudgetDocumentRepository.java
@@ -2,6 +2,14 @@ package com.veeteq.finance.budget.repository;
 
 import com.veeteq.finance.budget.model.BudgetDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BudgetDocumentRepository extends JpaRepository<BudgetDocument, Long> {
+
+  @Query(value = "SELECT DISTINCT bd.documentTitle from BudgetDocument bd WHERE lower(bd.documentTitle) LIKE LOWER(CONCAT('%', :pattern, '%')) ORDER BY bd.documentTitle",
+         nativeQuery = false)
+  List<String> findDistinctByDocumentTitleContainingIgnoreCase(@Param(value = "pattern") String pattern);
 }
